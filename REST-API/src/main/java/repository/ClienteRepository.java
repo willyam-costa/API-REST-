8d7def1287fd0d99e7f188433345f88daeeb7ad5
Victor Moraes <56882403+willyam-costa@users.noreply.github.com>
2021-08-13 11:35:22 -0300

    Add files via upload
    não encontra aplicação via localhost:8080/clientes

---
+++ REST-API/src/main/java/repository/ClienteRepository.java
@@ -0,0 +1,11 @@
+package repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import model.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+
+}
